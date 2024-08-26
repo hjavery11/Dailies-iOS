c5a19ee3b8743ee5edcace75a53a61e671899386
hjaver11 <hejavery@gmail.com>
2024-08-26 10:55:49 -0700

    Initial Commit

---
+++ Dailies/DailiesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DailiesApp.swift
+//  Dailies
+//
+//  Created by Harrison Javery on 8/26/24.
+//
+
+import SwiftUI
+
+@main
+struct DailiesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
